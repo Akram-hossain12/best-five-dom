2816a8497e739f2c0b09621548740bd11a44c173
Akram-hossain12 <akramhossain0502@gmail.com>
2022-08-23 13:10:57 +0600

    calculetfun

--- js/select.js
+++ js/select.js
@@ -12,7 +12,7 @@ function display(showElement){
        const player = bestFive[i].PlayersName;
        
        const ol = document.createElement('ol');
-
+        
     ol.innerHTML= `
        <li style = " color: white; list-style:none; font-size:24px;">${i+1} .${player} </li>
        `;
@@ -28,11 +28,91 @@ function display(showElement){
    }
   
 }
+// calculet area 
+
+    //  common function for innputFild 
+
+function getAllPlayerById (calculatPer){
+   const getElementFromInput =  document.getElementById(calculatPer);
+   const getAllPlayerCostString = getElementFromInput.value;
+   const getAllCostNumber = parseInt(getAllPlayerCostString);
+   
+  
+   // console.log(allPlayerCost)
+return  getAllCostNumber;
+}
+
+   //   common function for innerText 
+
+function commonInnerTextField (innerTextId){
+   const getInnerTextField = document.getElementById(innerTextId);
+   const getAllInnerTextString = getInnerTextField.innerText;
+   const getInnerNumber = parseInt(getAllInnerTextString);
+   return getInnerNumber;
+}
+
+document.getElementById('player-calculat').addEventListener('click', function(){
+   //   player per cost calculet  
+
+   const getInputForPlayer =  getAllPlayerById ("player-input-field");
+   const perlayerPer = getInputForPlayer;
+   const totlePlayerCost = perlayerPer * bestFive.length;
+   
+   // show in the valu innerTEXt 
+   
+   const getInnerTextField = document.getElementById("Player-expanse-totle");
+   const getAllInnerTextString = getInnerTextField.innerText;
+   const getInnerNumber = parseInt(getAllInnerTextString);
+   getInnerTextField.innerText =  totlePlayerCost;
+  
+   console.log(totlePlayerCost)
+  
+  })
+
+  function commonInnerTextField (innerTextId){
+   const getInnerTextField = document.getElementById(innerTextId);
+   const getAllInnerTextString = getInnerTextField.innerText;
+   const getInnerNumber = parseInt(getAllInnerTextString);
+   return getInnerNumber;
+}
+
+  document.getElementById('maneger-calculat').addEventListener('click', function(){
+
+   //  call all common function by id 
+
+   const ManegerCostById = getAllPlayerById ("maneger-input-field");
+  const cochlCostById =  getAllPlayerById ("coch-input-field");
+  const getInnerTextField = commonInnerTextField("Player-expanse-totle");
+ 
+//   all cost calculet area  
+
+  const getInputForPlayer =  getAllPlayerById ("player-input-field");
+  const perlayerPer = getInputForPlayer;
+  const totlePlayerCost = perlayerPer * bestFive.length;
+   const cochManegerPlayerCost = totlePlayerCost + ManegerCostById + cochlCostById;
+ 
+   
+
+//  totle cost calculet show totle expens area
+  
+  const getTotleExpenseField = document.getElementById("totle-expense");
+  const getAllInnerTextString = getInnerTextField.innerText;
+  const getInnerNumber = parseInt(getAllInnerTextString);
+  getTotleExpenseField.innerText = cochManegerPlayerCost;
+
+
+ })
+
+
+// calculet end 
+
+
 
 
 
 function getAllButtonClicked (elementId){
    const PlayersName = elementId.parentNode.children[0].innerText;
+   // getAllButtonClicked.this.disabeld = true;
    const PlayersObj = {
     PlayersName : PlayersName
    };
